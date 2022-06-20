25d6189012bef10d520f058a932881a83e14d3dd
Rajnish-Patel <76821795+Rajnish-Patel@users.noreply.github.com>
2022-06-20 13:42:36 +0530

    added contact and one sample.cpp
    Signed-off-by: Rajnish-Patel <76821795+Rajnish-Patel@users.noreply.github.com>

---
+++ Online Shopping Mart/Shopping Mart/sample.cpp/sample.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define quickly ios_base::sync_with_stdio(0);cin.tie(0)
+
+int32_t main()
+{
+    quickly;
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    
+
+    return 0;
+}
